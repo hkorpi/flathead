7ef72e225cfd1643aec5bff914f7670d6a3ca693
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2024-12-23 20:41:15 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -8,10 +8,10 @@
   :test-paths ["src/test/cljc" "src/test/clj"]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
-            [lein-ancient "0.6.15"]
+            [lein-ancient "0.7.0"]
             [lein-changelog "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [org.clojure/tools.namespace "1.0.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
+                                  [org.clojure/tools.namespace "1.5.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[flathead \"[0-9.]*\"\\\\]/[flathead \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
